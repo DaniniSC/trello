8d4952e380b8f3f28235b441fdbba3994330a012
DaniniSC <dpsantandercastro@gmail.com>
2016-11-08 21:26:01 -0300

    Se agrega logo y estilo en main.css, se oculta textarea

--- js/script.js
+++ js/script.js
@@ -19,7 +19,9 @@ function anadirLista() { //se ejecuta al hacer click en el botón de html AÑADI
 function guardarLista() {
 	//rescatar elementos de html
 	var divLista = document.getElementById("tablero").lastChild;
+	var inputTitulo = divLista.getElementsByTagName("input")[0];
 	var contenidoInputTitulo = divLista.getElementsByTagName("input")[0].value;
+	var btnInputTitulo = divLista.getElementsByTagName("button")[0];
 	//crear nuevos elementos
 	var crearh3 = document.createElement("h3");
 	var crearBtnAnadirTarea = document.createElement("button");
@@ -35,5 +37,56 @@ function guardarLista() {
 	//atributos	
 	crearDivTareas.setAttribute("class", "tareas");
 	crearBtnAnadirTarea.setAttribute("onclick", "anadirTarea()");
+	inputTitulo.setAttribute("class", "hidden");
+	btnInputTitulo.setAttribute("class", "hidden");
 }
 
+function anadirTarea() {
+	//rescatar elementos de html
+	var divLista = document.getElementById("tablero").lastChild;
+	var divTareas = divLista.lastChild;
+	//crear nuevos elementos
+	var crearTextarea = document.createElement("textarea");
+	var crearBtnGuardarTarea = document.createElement("button");
+	var nodoGuardarTarea = document.createTextNode("Guardar Tarea");
+	//hijos
+	divTareas.appendChild(crearTextarea);
+	divTareas.appendChild(crearBtnGuardarTarea);
+	crearBtnGuardarTarea.appendChild(nodoGuardarTarea);
+	//atributos
+	crearBtnGuardarTarea.setAttribute("onclick", "guardarTarea()");
+	btnAnadirTarea.setAttribute("class", "hidden");
+}
+
+function guardarTarea() {
+	//rescatar elementos de html
+	var divLista = document.getElementById("tablero").lastChild;
+	var divTareas = divLista.lastChild;
+	var textareaTarea = divTareas.getElementsByTagName("textarea")[0];
+	var contenidoTextarea = textareaTarea.value;
+	var btnTextareaTarea = divTareas.getElementsByTagName("button")[0];
+	//crear nuevos elementos
+	var crearDivTareaNueva = document.createElement("div");
+	var crearCheckbox = document.createElement("input");
+	var crearh4 = document.createElement("h4");
+	var crearBtnAnadirTarea = document.createElement("button");
+	var nodoh4 = document.createTextNode(contenidoTextarea);
+	var nodoAnadirTarea = document.createTextNode("Añadir Tarea");
+	//hijos
+	divTareas.appendChild(crearDivTareaNueva);
+	crearDivTareaNueva.appendChild(crearCheckbox);
+	crearDivTareaNueva.appendChild(crearh4);
+	crearDivTareaNueva.appendChild(crearBtnAnadirTarea);
+	crearh4.appendChild(nodoh4);
+	crearBtnAnadirTarea.appendChild(nodoAnadirTarea);
+	//atributos
+	crearDivTareaNueva.setAttribute("class", "tareaNueva");
+	crearCheckbox.setAttribute("type", "checkbox");
+	crearCheckbox.setAttribute("class", "pull-left media-middle");
+	crearh4.setAttribute("class", "pull-left");
+	crearBtnAnadirTarea.setAttribute("onclick", "anadirTarea()");
+	crearBtnAnadirTarea.setAttribute("class", "pull-right");
+	textareaTarea.classList.add("oculto");
+	btnTextareaTarea.setAttribute("class", "hidden");
+
+}
